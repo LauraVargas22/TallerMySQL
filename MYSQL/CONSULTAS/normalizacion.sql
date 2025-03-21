5b7f638b29562962f66545fda25819b21b3eedf4
Laura <181015028+LauraVargas22@users.noreply.github.com.>
2025-03-21 18:44:14 -0500

    feat: :sparkles: Normalización Datos

--- MYSQL/CONSULTAS/normalizacion.sql
+++ MYSQL/CONSULTAS/normalizacion.sql
@@ -1,4 +1,4 @@
---1. Crear una tabla HistorialPedidos que almacene cambios en los pedidos
+--Crear una tabla HistorialPedidos que almacene cambios en los pedidos
 --La tabla pedidos tendrá una relación 1:N con la tabla historialPedidos, además una relación N:N con la tabla detallespedido, por lo que se crea una tabla intermedia historialDetallesPedido.
 
 CREATE TABLE IF NOT EXISTS historialPedidos (
@@ -25,8 +25,6 @@ CREATE TABLE IF NOT EXISTS historialDetallesPedido(
 )
 
 -- 2. Evaluar la tabla Clientes para eliminar datos redundantes y normalizar hasta 3NF.
--- Se eliminó el atributo nombre, ya que es un valor atómico y se crearon atributos separados para el primer y segundo nombre, así como para el primer y segundo apellido.
---Con esto se evita la redundancia de datos y se normaliza la tabla hasta la tercera forma normal.
 ALTER TABLE clientes DROP COLUMN nombre;
 ALTER TABLE clientes ADD COLUMN primer_nombre VARCHAR(50);
 ALTER TABLE clientes ADD COLUMN segundo_nombre VARCHAR(50) NULL;
@@ -85,3 +83,86 @@ CREATE TABLE IF NOT EXISTS ciudad(
 
 ALTER TABLE ubicacioncliente ADD COLUMN ciudad_id INT;
 ALTER TABLE ubicacioncliente ADD FOREIGN KEY (ciudad_id) REFERENCES ciudad(id);
+
+-- 5. Normalizar Proveedores para tener ContactoProveedores en otra tabla.
+-- Se elimina el atributo contacto de la tabla proveedores y se crea la tabla ContactoProveedores.
+ALTER TABLE Proveedores DROP COLUMN contacto;
+
+CREATE TABLE IF NOT EXISTS ContactoProveedor(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    proveedor_id INT,
+    contacto VARCHAR(100),
+    FOREIGN KEY (proveedor_id) REFERENCES Proveedores(id)
+)
+
+--Se normaliza el nombre del proveedor
+ALTER TABLE Proveedores DROP COLUMN nombre;
+ALTER TABLE Proveedores ADD COLUMN nombre VARCHAR(50);
+ALTER TABLE Proveedores ADD COLUMN apellido VARCHAR(50);
+
+--6. Crear una tabla de Telefonos para almacenar múltiples números por cliente
+
+CREATE TABLE IF NOT EXISTS Telefonos(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    cliente_id INT,
+    telefono VARCHAR(20),
+    FOREIGN KEY (cliente_id) REFERENCES Clientes(id)
+)
+
+--7. Transformar TiposProductos en una relación categórica jerárquica
+--Una relación categórica jerárquica me permite definir la relación padre-hijo entre los tipos de productos.
+
+ALTER TABLE TiposProductos ADD COLUMN categoria_padre_id INT NULL;
+ALTER TABLE TiposProductos ADD FOREIGN KEY (categoria_padre_id) REFERENCES TiposProductos(id);
+
+--8. Normalizar Pedidos y DetallesPedido para evitar inconsistencias de precios.
+--El atributo total en Pedidos puede tener inconsistencias por lo que este podira ser calculado a partir de los detalles de pedido.
+ALTER TABLE Pedidos DROP COLUMN total;
+
+--9. Usar una relación de muchos a muchos para Empleados y Proveedores .
+--Al tener una relación de michos a muchos se crea una tabla intermedia EmpleadosProveedores que las relacione.
+
+CREATE TABLE IF NOT EXISTS EmpleadosProveedores(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    empleado_id INT,
+    proveedor_id INT,
+    FOREIGN KEY (empleado_id) REFERENCES Empleados(id),
+    FOREIGN KEY (proveedor_id) REFERENCES Proveedores(id)
+)
+
+--10. Convertir la tabla UbicacionCliente en una relación genérica de Ubicaciones.
+--Se crea una tabla de ubicaciones que almacene las ubicaciones de los clientes, empleados y proveedores.
+
+CREATE TABLE IF NOT EXISTS Ubicaciones(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    direccion VARCHAR(255),
+    ciudad_id INT,
+    codigo_postal VARCHAR(10),
+    FOREIGN KEY (ciudad_id) REFERENCES ciudad(id)
+)
+
+DROP TABLE UbicacionCliente;
+
+--Crear tabla para proveedor
+CREATE TABLE IF NOT EXISTS proveedorUbicacion(
+    proveedor_id INT,
+    ubicacion_id INT,
+    FOREIGN KEY (proveedor_id) REFERENCES Proveedores(id),
+    FOREIGN KEY (ubicacion_id) REFERENCES Ubicaciones(id)
+)
+
+--Crear tabla para empleados
+CREATE TABLE IF NOT EXISTS empleadoUbicacion(
+    empleado_id INT,
+    ubicacion_id INT,
+    FOREIGN KEY (empleado_id) REFERENCES Empleados(id),
+    FOREIGN KEY (ubicacion_id) REFERENCES Ubicaciones(id)
+)
+
+--Crear tabla para clientes
+CREATE TABLE IF NOT EXISTS clienteUbicacion(
+    cliente_id INT,
+    ubicacion_id INT,
+    FOREIGN KEY (cliente_id) REFERENCES Clientes(id),
+    FOREIGN KEY (ubicacion_id) REFERENCES Ubicaciones(id)
+)
